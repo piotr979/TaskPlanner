bc698e4700c1d4d7313a795ad0af87dbf6ba1db8
Piotr Glaza <peterglaza@gmail.com>
2020-03-25 16:44:19 +0000

    Initial Commit

---
+++ TaskPlanner/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TaskPlanner
+//
+//  Created by Start on 25/03/2020.
+//  Copyright © 2020 Start. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TaskPlanner/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TaskPlanner
+//
+//  Created by Start on 25/03/2020.
+//  Copyright © 2020 Start. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
